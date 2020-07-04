81850972260fea48931e773e69e5812be7c72a51
andrea <andrea.cristina.dev@gmail.com>
2020-07-04 08:59:18 +0300

    First Commit.

---
+++ web/src/App.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+export default function App () {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    axios.get('/api/names')
+      .then(res => {
+        console.log(res)
+        setData(0);
+    }).catch((err) => {console.log(err)})
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick = {() => setCount(count + 1)}>
+        Clicked
+      </button>
+      <p>here: {data}</p>
+    </div>
+  );
+}
