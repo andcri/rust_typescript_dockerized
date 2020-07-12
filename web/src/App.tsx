cc85b0cd1c509461c791cd61bff6b57014b7924c
andrea <andrea.cristina.dev@gmail.com>
2020-07-12 23:44:28 +0300

    Add first functionalities.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,31 +3,72 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import './assets/main.css'
+import {Button, Toast} from 'react-bootstrap';
 
 export default function App () {
-  const [count, setCount] = useState(0);
   const [data, setData] = useState(0);
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     axios.get('/api')
       .then(res => {
         console.log(res)
         setData(0);
+        console.log(data)
     }).catch((err) => {console.log(err)})
   }, []);
 
+  function sendRequest () {
+    axios.post('/api/post', {
+      'username': text
+    }).then(res => {
+      setResult(res.data) 
+    })
+
+  } 
+
+  function copied () {
+    navigator.clipboard.writeText(result)
+    setShow(true)
+  }
+
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick = {() => setCount(count + 1)}>
-        Clicked
-      </button>
-      <p>here test: {data}</p>
-
-     <div className="font-bold text-purple-500 text-xl mb-2">
-          Test
+      <div className="container">
+        <div className="row">
+        <div className="col-sm"/>
+        <div className="col-sm">
+          <h1>Sluggifier</h1>
+          <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide={true}>
+            <Toast.Header>
+              <strong className="mr-auto">Success!</strong>
+            </Toast.Header>
+            <Toast.Body>Copied {result} to clipboard</Toast.Body>
+          </Toast>
+          <p/>
         </div>
+        <div className="col-sm"/>
+        </div>
+      </div>
+      <div className="container">
+      <div className="row">
+          <div className="col-sm"/>
+          <div className="col-sm">
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" onChange={e => setText(e.target.value)}/>
+            </div>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" value={result} readOnly={true}/>
+            </div>
+            <Button onClick={sendRequest}>Slugify</Button>{' '}
+            <Button variant="danger" onClick={() => setResult('')}>Reset</Button>{' '}
+            <Button variant="warning" onClick={copied}>Copy</Button>
+          </div>
+          <div className="col-sm"/>
+        </div>
+      </div>
     </div>
   );
 }
