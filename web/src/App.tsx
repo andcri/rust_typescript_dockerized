ba3a4e37a3093f53e81d16691396eafeff2e87c5
andrea <andrea.cristina.dev@gmail.com>
2020-07-12 21:07:18 +0300

    Add post request.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,21 +6,33 @@ import './App.css';
 
 export default function App () {
   const [count, setCount] = useState(0);
-  const [data, setData] = useState(0);
+  const [data, setData] = useState("");
 
   useEffect(() => {
     axios.get('/api')
       .then(res => {
         console.log(res)
-        setData(0);
+        setData(res.data);
     }).catch((err) => {console.log(err)})
   }, []);
+
+  function makeRequest () {
+    axios.post('/api/post', {
+      username: "65 this is a test"
+    })
+    .then(res => {
+      setData(res.data)
+    })
+  }
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick = {() => setCount(count + 1)}>
         Clicked
+      </button>
+      <button onClick = {() => makeRequest()}>
+        Click MEEEEEE
       </button>
       <p>here test: {data}</p>
     </div>
