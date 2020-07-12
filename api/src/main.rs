e776198b9c5f9261e374e2ccbbd01c906d51b594
andrea <andrea.cristina.dev@gmail.com>
2020-07-12 21:10:43 +0300

    Add bootsrtap.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, Result};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use listenfd::ListenFd;
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-struct Info {
-    username: String,
-}
 
 async fn index(_req: HttpRequest) -> impl Responder {
     "Hello World!"
@@ -13,30 +7,12 @@ async fn index(_req: HttpRequest) -> impl Responder {
 async fn index2(_req: HttpRequest) -> impl Responder {
     "Hello World! again"
 }
-async fn index3(info: web::Json<Info>) -> Result<String> {
-    let test = &info.username;
-    let mut fin = String::new();
-    for c in test.chars(){
-        if c == ' '{
-            fin.push('-')
-        } else if c.is_numeric() {
-            fin.push(c)
-        } else if c.is_uppercase() {
-            let chars: Vec<char> = c.to_lowercase().to_string().chars().collect();
-            fin.push(chars[0])
-        } else {
-            fin.push(c)
-        }
-    }
-    Ok(format!("{}", fin))
-}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new(|| App::new()
         .route("/api", web::get().to(index))
-        .route("/api/post", web::post().to(index3))
         .route("/api/again", web::get().to(index2)));
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,27 +3,19 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import './assets/main.css'
 
 export default function App () {
   const [count, setCount] = useState(0);
-  const [data, setData] = useState("");
+  const [data, setData] = useState(0);
 
   useEffect(() => {
     axios.get('/api')
       .then(res => {
         console.log(res)
-        setData(res.data);
+        setData(0);
     }).catch((err) => {console.log(err)})
   }, []);
-
-  function makeRequest () {
-    axios.post('/api/post', {
-      username: "65 this is a test"
-    })
-    .then(res => {
-      setData(res.data)
-    })
-  }
 
   return (
     <div>
@@ -31,10 +23,11 @@ export default function App () {
       <button onClick = {() => setCount(count + 1)}>
         Clicked
       </button>
-      <button onClick = {() => makeRequest()}>
-        Click MEEEEEE
-      </button>
       <p>here test: {data}</p>
+
+     <div className="font-bold text-purple-500 text-xl mb-2">
+          Test
+        </div>
     </div>
   );
 }
